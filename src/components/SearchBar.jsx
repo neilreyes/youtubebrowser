95bf563eb83c14caed65d9fd4fce2ffbb2b88b2f
Neil Reyes <rayneilreyes@gmail.com>
2018-11-22 20:47:56 +0800

    Added input attributes and function handler

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            term: ''
+        }
+
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    handleSearch(e){
+        const target = e.target;
+        const name = target.name;
+        const value = target.value;
+        
+        this.setState({
+            [name]: value,
+        })
+    }
+
     render() {
         return(
             <div>
-                SearchBar
+                <input
+                    name="term"
+                    value={this.state.term}
+                    className="SearchBar"
+                    placeholder="Search term..."
+                    onChange={this.handleSearch}
+                />
             </div>
         );
     }
