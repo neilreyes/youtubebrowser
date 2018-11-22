0834d3bd192091c44bfc1cd20b90357ae0093fd9
Neil Reyes <rayneilreyes@gmail.com>
2018-11-22 20:58:38 +0800

    Converted function base component App to class base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
+import searchYoutube from 'youtube-api-v3-search';
 
 // Components 
 import SearchBar from './SearchBar';
 
-const YOUTUBE_API_KEY = "AIzaSyALVDWH0Y6hEuqurGEM_qcGzKtKJ1qeIrY";
+const YOUTUBE_API_KEY = "AIzaSyC_vTCoR8PseScF4owFyskryB2kZ3jlz1M";
 
-class App extends Component{
-    render() {
+const searchQuery = {
+    q: 'nodejs',
+    type: 'video',
+    videoType: 'any'
+}
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state ={
+            videos: []
+        }
+
+        searchYoutube(
+            YOUTUBE_API_KEY,
+            searchQuery, 
+            (error, result) => {
+                if(error){
+                    console.log('Error: ', error);
+                } else {
+                    this.setState({
+                        videos: result.items,
+                    });
+
+                    console.log('Videos: ', this.state.videos);
+                }
+            }
+        )
+    }
+
+    render(){
         return(
             <div>
-                App
                 <SearchBar />
             </div>
-        );
+        )
     }
 }
 
